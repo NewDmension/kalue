d19d1454982541380bd7f310f5bf66c06b8c86d3
Sergi <info@newdmension.com>
2026-01-24 19:28:23 +0100

    kalue9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import LoginClient from './LoginClient';
 
-export default function Page() {
+export default function HomePage() {
   return (
     <div className="min-h-screen w-full px-4 py-10 text-white flex items-center justify-center">
       <LoginClient />
